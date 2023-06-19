a48b416b480a4565e27835fdd6917f8cbebe86ee
sun-17 <134663272+sun-17@users.noreply.github.com>
2023-06-19 20:32:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+$("img").click(function(){
+    
+var aud=new Audio("./move.mp3");
+aud.play();
+$(".item2").html("<p>Santhosh    🤣</p>");
+$(".item2").css("color","rgb(87, 87, 57)");
+$(".item2").css("font-weight","bold");
+$(".item2").css("font-size","70px");
+$(".item4").html("<p>it's  just for fun..don't take it serious</p>");
+
+});
+
+var aud=new Audio("./move.mp3");
+aud.play();
